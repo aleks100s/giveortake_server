62eca3c28db8d90d8d70f32b2b7755aeea84946a
aleks100s <aleksandr100s@yandex.ru>
2024-05-10 23:41:20 +0700

    Add random question endpoint

--- Sources/App/Controllers/QuestionController.swift
+++ Sources/App/Controllers/QuestionController.swift
@@ -4,8 +4,17 @@ struct QuestionController: RouteCollection {
 	func boot(routes: RoutesBuilder) throws {
 		let questions = routes.grouped(PathComponent(stringLiteral: Question.schema))
 
+		questions.get(use: { try await self.getRandom(req: $0) })
 		questions.post(use: { try await self.create(req: $0) })
 		questions.delete(":ID", use: { try await self.delete(req: $0) })
+	}
+	
+	func getRandom(req: Request) async throws -> QuestionDTO {
+		guard let question = try await Question.query(on: req.db).all().randomElement() else {
+			throw Abort(.noContent)
+		}
+		
+		return question.toDTO()
 	}
 	
 	func create(req: Request) async throws -> QuestionDTO {
